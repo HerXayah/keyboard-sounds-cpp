0ffd4bc7e9985c7053551eda3a5d800fc34d4fe7
NULLYUKI <>
2021-11-13 22:24:19 +0100

    Saving current progress, will still be changed with JSON.

--- osu-keyboard-sounds.cpp
+++ osu-keyboard-sounds.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <Windows.h>
 #include "bass.h"
+#include <vector>
 
 using namespace std;
 
@@ -9,8 +10,9 @@ HHOOK hHook = NULL;
 bool show = false;
 bool on = true;
 
+
 // to turn off the Input detection
-static int yeet = 1;
+DWORD current_key_down = NULL;
 
 HSTREAM keyCaps;
 HSTREAM keyConfirm;
@@ -21,80 +23,18 @@ HSTREAM keyPress2;
 HSTREAM keyPress3;
 HSTREAM keyPress4;
 
+vector<HSTREAM*> keyPressSounds = { &keyPress1, &keyPress2, &keyPress3, &keyPress4 };
+
 LRESULT CALLBACK MyHook(int nCode, WPARAM wParam, LPARAM lParam) {
 
     if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)
     {
         PKBDLLHOOKSTRUCT pKey = (PKBDLLHOOKSTRUCT)lParam;
         SHORT keyState = GetAsyncKeyState(pKey->vkCode);
-        if (keyState >= 0)
-        switch (pKey->vkCode)
-        {
-        case VK_F7:
-            on = !on;
-            if (!on) {
-
-               return yeet = 0;
-               
-            }
-            else {
-
-               return yeet = 1;
-
-            }
-        case VK_CAPITAL:
-            if (yeet == 1) {
-
-
-                BASS_ChannelPlay(keyCaps, TRUE);
-            }
-            break;
-        case VK_RETURN:
-            if (yeet == 1) {
-                BASS_ChannelPlay(keyConfirm, TRUE);
-            }
-            break;
-        case VK_BACK:
-            if (yeet == 1) {
-                BASS_ChannelPlay(keyDelete, TRUE);
-            }
-            break;
-        case VK_LEFT:
-        case VK_UP:
-        case VK_RIGHT:
-        case VK_DOWN:
-            if (yeet == 1) {
-                BASS_ChannelPlay(keyMovement, TRUE);
-            }
-            break;
-        case VK_F8:
-            show = !show;
-            if (!show)
-            {
-                ShowWindow(GetConsoleWindow(), SW_SHOW);
-
-            }
-            else {
-                ShowWindow(GetConsoleWindow(), SW_HIDE);
-            }
-        default:
-            if (yeet == 1) {
-                switch (rand() % 4)
-                {
-                case 0:
-                    BASS_ChannelPlay(keyPress1, TRUE);
-                    break;
-                case 1:
-                    BASS_ChannelPlay(keyPress2, TRUE);
-                    break;
-                case 2:
-                    BASS_ChannelPlay(keyPress3, TRUE);
-                    break;
-                case 3:
-                    BASS_ChannelPlay(keyPress4, TRUE);
-                    break;
-                };
-            };
+        current_key_down = pKey->vkCode;
+        if (keyState >= 0) {
+            unsigned int x = rand() % keyPressSounds.size();
+            BASS_ChannelPlay(*keyPressSounds[x], true);            
         } return CallNextHookEx(hHook, nCode, wParam, lParam);
     }
 }
@@ -110,10 +50,11 @@ int main()
     keyConfirm = BASS_StreamCreateFile(FALSE, "./sounds/keyConfirm.mp3", 0, 0, 0);
     keyDelete = BASS_StreamCreateFile(FALSE, "./sounds/keyDelete.mp3", 0, 0, 0);
     keyMovement = BASS_StreamCreateFile(FALSE, "./sounds/keyMovement.mp3", 0, 0, 0);
-    keyPress1 = BASS_StreamCreateFile(FALSE, "./sounds/keyPress1.mp3", 0, 0, 0);
-    keyPress2 = BASS_StreamCreateFile(FALSE, "./sounds/keyPress2.mp3", 0, 0, 0);
-    keyPress3 = BASS_StreamCreateFile(FALSE, "./sounds/keyPress3.mp3", 0, 0, 0);
-    keyPress4 = BASS_StreamCreateFile(FALSE, "./sounds/keyPress4.mp3", 0, 0, 0);
+    keyPress1 = BASS_StreamCreateFile(FALSE, "./sounds/keyPress1.wav", 0, 0, 0);
+    keyPress2 = BASS_StreamCreateFile(FALSE, "./sounds/keyPress2.wav", 0, 0, 0);
+    keyPress3 = BASS_StreamCreateFile(FALSE, "./sounds/keyPress3.wav", 0, 0, 0);
+    keyPress4 = BASS_StreamCreateFile(FALSE, "./sounds/keyPress4.wav", 0, 0, 0);
+    
 
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
