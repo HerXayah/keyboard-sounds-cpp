25e071dbc12fe05650aa289ffc1dab7ab632cfdf
Lilly <justsweetluna@gmail.com>
2021-11-15 14:56:17 +0100

    oof im retard

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@
 #include <wchar.h>
 #include <traypp/tray.hpp>
 
-
 bool logger_proc(unsigned int level, const char* format, ...) {
     bool status = false;
 
@@ -51,24 +50,9 @@ void dispatch_proc(uiohook_event* const event) {
     case EVENT_KEY_PRESSED:
         // If the escape key is pressed, naturally terminate the program.
         if (event->data.keyboard.keycode == VC_F7) {
-            int status = hook_stop();
-            switch (status) {
-            case UIOHOOK_SUCCESS:
-                break;
 
-            case UIOHOOK_ERROR_OUT_OF_MEMORY:
-                logger_proc(LOG_LEVEL_ERROR, "Failed to allocate memory. (%#X)", status);
-                break;
+            break;
 
-            case UIOHOOK_ERROR_X_RECORD_GET_CONTEXT:
-                logger_proc(LOG_LEVEL_ERROR, "Failed to get XRecord context. (%#X)", status);
-                break;
-
-            case UIOHOOK_FAILURE:
-            default:
-                logger_proc(LOG_LEVEL_ERROR, "An unknown hook error occurred. (%#X)", status);
-                break;
-            }
         }
     case EVENT_KEY_RELEASED:
         snprintf(buffer + length, sizeof(buffer) - length,
@@ -89,7 +73,7 @@ void dispatch_proc(uiohook_event* const event) {
 }
 
 int hookstatus(bool stateOn) {
-    if (stateOn = true) {
+    if (stateOn == true) {
         hook_run();
     }
     else {
@@ -104,18 +88,13 @@ int main() {
     tray.addEntry(Tray::Button("Exit", [&] { tray.exit(); }));
     tray.addEntry(Tray::Button("Test"))->setDisabled(true);
     tray.addEntry(Tray::Separator());
-    tray.addEntry(Tray::Toggle("Test Toggle", true, [](bool stateOn) { hookstatus(stateOn); printf("Deine Mum: %i\n", stateOn); }));
-    tray.addEntry(Tray::Toggle("Test Toggle", false, [](bool state) { printf("State: %i\n", state); }));
+    tray.addEntry(Tray::Toggle("toogle", true, [](bool stateOn) { hookstatus(stateOn); printf("Deine Mum: %i\n", stateOn); }));
     tray.addEntry(Tray::Separator());
     tray.addEntry(Tray::Submenu("Test Submenu"))->addEntry(Tray::Button("Submenu button!"))->setDisabled(true);
 
     hook_set_logger_proc(&logger_proc);
     hook_set_dispatch_proc(&dispatch_proc);
 
+    hook_run(); // Starting State On
     tray.run();
-
-    //Debug 
-/*  int status = hook_run();
-
-    return status; */
 }
